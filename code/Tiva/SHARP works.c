47635932ca13267cd54111d81d4a8a394d54e0a8
4m4npr33t <aman06singh@gmail.com>
2016-07-07 21:03:59 +0530

    ADC

---
+++ code/Tiva/SHARP works.c
@@ -0,0 +1,35 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "inc/hw_gpio.h"
+#include "inc/hw_adc.h"
+#include "driverlib/debug.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/adc.h"
+#include "driverlib/gpio.h"
+
+int Temp;
+
+int main(void)
+{
+	uint32_t ui32ADC0Value[1];
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
+	GPIOPinTypeADC(GPIO_PORTE_BASE, GPIO_PIN_3);
+	SysCtlClockSet(SYSCTL_SYSDIV_5|SYSCTL_USE_PLL|SYSCTL_OSC_MAIN|SYSCTL_XTAL_16MHZ);
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);
+	ADCSequenceConfigure(ADC0_BASE, 3, ADC_TRIGGER_PROCESSOR, 0);
+	ADCSequenceStepConfigure(ADC0_BASE,3,0,ADC_CTL_CH0|ADC_CTL_IE|ADC_CTL_END);
+	ADCSequenceEnable(ADC0_BASE, 3);
+	while(1)
+	{
+		ADCIntClear(ADC0_BASE, 3);
+		ADCProcessorTrigger(ADC0_BASE, 3);
+		while(!ADCIntStatus(ADC0_BASE, 3, false))
+		{
+		}
+		ADCSequenceDataGet(ADC0_BASE, 3, ui32ADC0Value);
+		Temp = ui32ADC0Value[0] ;
+
+	}
+}
